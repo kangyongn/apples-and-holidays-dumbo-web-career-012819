9674d5fb1c0a5b9e85c2006eee0e524c2333d5f8
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-15 05:24:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,7 +32,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     
     holiday.each do |day, item|
-      arr = day.to_s.split
+      arr = day.to_s
+      arr.split
       arr.each {|word| word.capitalize!}
       capitalized = arr.join(" ")
       list = item.join(", ")
